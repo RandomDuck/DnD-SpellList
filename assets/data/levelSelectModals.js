926d543e7d1e7eee7e6bab4a3775cc09492389aa
Johannes Ström <johannes.strom@svenskaspel.se>
2021-03-02 12:57:44 +0100

    style fix and assigned generated keys to selector

--- assets/data/levelSelectModals.js
+++ assets/data/levelSelectModals.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Modal, TouchableOpacity, Text, TouchableWithoutFeedback} from 'react-native';
 
-function LevelOpen({controller,title,pretext}){
+function LevelOpen({controller,title}){
     return(
         <TouchableOpacity style={styles.levelOpen} onPress={()=>controller(true)}>
             <Text style={styles.spacer}>{title}</Text>
@@ -22,9 +22,9 @@ function LevelModal({show,controller,spellController,names,nameValues=undefined}
     let values=[];
     for (let i=0; i<names.length; i++) {
         let z = nameValues != undefined ? nameValues[i] : i;
-        values.push({name:names[i], value:z});
+        values.push({name:names[i], value:z, key:i});
     }
-    values.forEach(i=>selectors.push(<Selector controller={controller} spellController={spellController} name={i.name} value={i.value}/>))
+    values.forEach(i=>selectors.push(<Selector controller={controller} spellController={spellController} name={i.name} value={i.value} key={i.key}/>))
     return(
         <Modal
         animationType='slide'
@@ -55,9 +55,9 @@ const styles=StyleSheet.create({
 
     },
     spacer:{
-        padding:'2%',
+        padding:10,
         textAlign:'center',
-        margin:'2%',
+        margin:5,
         backgroundColor:'#222',
         color:'#fff'
     },
